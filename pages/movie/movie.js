01487920817dc48df54845200199f04ef9f6fe5d
taoaiyi1108 <mm18575206484@163.com>
2017-12-02 12:22:18 +0800

    首次提交

---
+++ pages/movie/movie.js
@@ -0,0 +1,86 @@
+// pages/movie/movie.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+    ],
+    hotData:[]
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'http://api.douban.com/v2/movie/in_theaters', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/text' // 默认值
+      },
+      method:"GET",
+      success: function (res) {
+        that.setData({
+          hotData:res.data.subjects
+        })
+       
+        console.log(_this.hotData)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
